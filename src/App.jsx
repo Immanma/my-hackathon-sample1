842606d3d9179d0a834b23084bccc1378be20671
immaculata chidimma <immaculata2016@gmail>
2024-06-22 06:10:56 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="header">
+  <h1>Take A Break !</h1>
+  <p>Pause, Refresh, Think... Your Perfect Break Awaits</p>
+  
+  <img src='https://cdn.outsideonline.com/wp-content/uploads/2023/05/take-a-break_s.png'/>
+
+    </div>
+    
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+
+<div className ="Header">
+  <h1>Take A Break !</h1>
+  <p>Paulse, Refresh, Think... Your Perfect Break Awaits</p>
+</div>
+
+};
+
+
+export default Header;
